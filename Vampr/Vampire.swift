c9f3e529d9831c8f28c702c772fc70696bbbb8ce
Chris Dean <chrisjgdean@gmail.com>
2018-03-15 13:09:40 -0700

    On closest ancestor test

--- Vampr/Vampire.swift
+++ Vampr/Vampire.swift
@@ -34,27 +34,57 @@ class Vampire {
   
   /// Adds the vampire as an offspring of this vampire
   func add(offspring: Vampire) {
-  
+    self.offspring.append(offspring)
+    offspring.creator = self
   }
   
   /// The total number of vampires created by that vampire
   var numberOfOffspring: Int {
-    return -1
+    return self.offspring.count
   }
   
   /// Returns the number of vampires away from the original vampire this vampire is
   var numberOfVampiresFromOriginal: Int {
-    return -1
+    var numberFromOV = 0
+    var currentOffspring = self
+    
+    while true {
+      guard let creator = currentOffspring.creator else {break}
+      numberFromOV += 1
+      currentOffspring = creator
+    }
+    return numberFromOV
   }
   
   /// Returns true if this vampire is more senior than the other vampire. (Who is closer to the original vampire)
   func isMoreSenior(than vampire: Vampire) -> Bool {
+    if self.numberOfVampiresFromOriginal < vampire.numberOfVampiresFromOriginal {
+      return true
+    }
     return false
   }
   
   // MARK: Stretch 
   
   /**
+   
+   
+   Node lca(Node node, int n1, int n2)
+   {
+   if (node == null)
+   return null;
+   
+   // If both n1 and n2 are smaller than root, then LCA lies in left
+   if (node.data > n1 && node.data > n2)
+   return lca(node.left, n1, n2);
+   
+   // If both n1 and n2 are greater than root, then LCA lies in right
+   if (node.data < n1 && node.data < n2)
+   return lca(node.right, n1, n2);
+   
+   return node;
+   }
+   
     Returns the closest common ancestor of two vampires.
     The closest common anscestor should be the more senior vampire if a direct ancestor is used.
  
@@ -63,6 +93,10 @@ class Vampire {
       * when comparing Ansel and Andrew, Ansel is the closest common anscestor.
    */
   func closestCommonAncestor(vampire: Vampire) -> Vampire {
+    
+    guard let creator = vampire.creator else {return vampire}
+    
+    //if 
     return vampire
   }
 }
